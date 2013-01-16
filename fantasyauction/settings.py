7a93b1db5330a0cf73dd18827da4a8a2b640ca8c
Roger Wang <r.rj.wang@gmail.com>
2013-01-15 23:47:42 -0600

    Update PROJECT_ROOT and add apps directory to python path

--- fantasyauction/settings.py
+++ fantasyauction/settings.py
@@ -1,10 +1,14 @@
 # Django settings for fantasyauction project.
 import os
+import sys
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../')
+
+#add apps directory to python path
+sys.path.append(os.path.join(PROJECT_ROOT, 'apps/'))
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -109,7 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, '../templates/'),
+    os.path.join(PROJECT_ROOT, 'templates/'),
 )
 
 INSTALLED_APPS = (
