81ca1028d9f7b3bedb08d1dacc36ef483b14070c
Roger Wang <r.rj.wang@gmail.com>
2013-01-17 10:31:30 -0600

    Add Roster model and add fields to League model

--- apps/league/models.py
+++ apps/league/models.py
@@ -1,4 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class League(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True)
+    users = models.ManyToManyField(User, blank=True)
+    size = models.IntegerField(default=0)
+
+class Roster(models.Model):
+    league = models.ForeignKey(League)
+    user = models.ForeignKey(User)
