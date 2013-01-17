7e3c73c4ab5afd198d651a86b823a4d31c4ce0fd
Roger Wang <r.rj.wang@gmail.com>
2013-01-17 16:21:39 -0600

    Add RosterPlayer model

--- apps/league/admin.py
+++ apps/league/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from league.models import League, Roster
+from league.models import League, Roster, RosterPlayer
 
 admin.site.register(League)
 admin.site.register(Roster)
+admin.site.register(RosterPlayer)
--- apps/league/models.py
+++ apps/league/models.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from player.models import Player
+
 class League(models.Model):
     name = models.CharField(max_length=100, blank=True)
     users = models.ManyToManyField(User, blank=True)
-    size = models.IntegerField(default=0)
+    size = models.IntegerField(blank=True)
 
 class Roster(models.Model):
     league = models.ForeignKey(League)
     user = models.ForeignKey(User)
+
+class RosterPlayer(models.Model):
+    roster = models.ForeignKey(Roster)
+    player = models.ForeignKey(Player)
